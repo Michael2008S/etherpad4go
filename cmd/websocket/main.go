59e0bcde935e2c1a78e1d444538d4a91b518856d
michael <michael2008s@gmail.com>
2020-03-06 11:59:06 +0800

    feat: add pad html page

--- cmd/websocket/main.go
+++ cmd/websocket/main.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 
 	poker "github.com/Michael2008S/etherpad4go"
+	"github.com/gorilla/mux"
 )
 
 var addr = flag.String("addr", ":8800", "http service address")
@@ -20,17 +21,27 @@ func serveHome(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		return
 	}
-	http.ServeFile(w, r, "home.html")
+	// http.ServeFile(w, r, "home.html")
+	http.ServeFile(w, r, "./template/index.html")
+}
+
+func servePad(w http.ResponseWriter, r *http.Request) {
+	log.Println(r.URL)
+	http.ServeFile(w, r, "./template/pad.html")
 }
 
 func main() {
 	flag.Parse()
 	hub := poker.NewHub()
 	go hub.Run()
-	http.HandleFunc("/", serveHome)
-	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+	r := mux.NewRouter()
+	r.HandleFunc("/", serveHome)
+	r.HandleFunc("/p/{name}", servePad)
+	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./template/static"))))
+	r.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
 		poker.ServeWs(hub, w, r)
 	})
+	http.Handle("/", r)
 	err := http.ListenAndServe(*addr, nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/Michael2008S/etherpad4go
 
 go 1.13
 
-require github.com/gorilla/websocket v1.4.1
+require (
+	github.com/gorilla/mux v1.7.4
+	github.com/gorilla/websocket v1.4.1
+)
