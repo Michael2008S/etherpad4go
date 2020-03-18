08b31fc0518b6cdc3d39200b3ab749159d57c313
michael <michael2008s@gmail.com>
2020-03-18 10:36:46 +0800

    feat: add attribpool test code

--- etherpad-lite/src/static/js/Changeset_test.js
+++ etherpad-lite/src/static/js/Changeset_test.js
@@ -24,3 +24,15 @@ aOp = opiterator.next();
 console.log(aOp);
 
 
+var AttribPool = require("./AttributePool");
+var apool = new(AttribPool);
+console.log(apool);
+apool.fromJsonable({"numToAttrib":{"0":["author","a.kVnWeomPADAT2pn9"],"1":["bold","true"],"2":["italic","true"]},"nextNum":3});
+console.log(apool);
+console.log(apool.getAttrib(1));
+
+var atext = {"text":"bold text\nitalic text\nnormal text\n\n","attribs":"*0*1+9*0|1+1*0*1*2+b|1+1*0+b|2+2"};
+console.log(atext);
+var opiterator = Changeset.opIterator(atext.attribs);
+console.log(opiterator);
+console.log(opiterator.next());
